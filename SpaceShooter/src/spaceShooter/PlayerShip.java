324ab2243e21838360022ce894c01b7fc2b76189
Pavel Czempin <p.czempin@gmx.de>
2013-03-10 14:27:25 +0100

    added new Constructer with more paramters to PlayerShip

--- SpaceShooter/src/spaceShooter/PlayerShip.java
+++ SpaceShooter/src/spaceShooter/PlayerShip.java
@@ -3,13 +3,15 @@ package spaceShooter;
 import org.newdawn.slick.GameContainer;
 
 public class PlayerShip extends Ship {
-	
-	public PlayerShip(GameContainer container, int x, int y) {
-		super(container, "/data/PlayerShip1.png", x, y, 0.0002f, 0.0001f, 1.0f);
-		//this.rotationSpeed = 0;
+
+	public PlayerShip(GameContainer container, int x, int y, float rotationAcceleration,
+			float acceleration, float scale) {
+		super(container, "/data/PlayerShip1.png", x, y, rotationAcceleration, acceleration, scale);
+		// this.rotationSpeed = 0;
 	}
 	
-	
-	
-	
+	public PlayerShip(GameContainer container, int x, int y) {
+		this(container,  x, y, 0.0002f, 0.0001f, 1.0f);
+	}
+
 }
